068b343db4433befed6a6b5ced52160113f96e55
InVanSav <savickijivan11@gmail.com>
2023-07-06 16:49:25 +1000

    Added empty interfaces of repositories

---
+++ Backend/Backend.Database/Interfaces/IAuctionRepository.cs
@@ -0,0 +1,5 @@
+﻿using Backend.Domain.Entity;
+
+namespace Backend.Database.Interfaces;
+
+public interface IAuctionRepository : IBaseRepository<Auction> { }
---
+++ Backend/Backend.Database/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Database.Interfaces;
+
+public interface IBaseRepository<T> {
+    public Task<bool> Create(T entity);
+
+    public Task<T> Get(int id);
+
+    Task<List<T>> Select();
+
+    public Task<bool> Delete(T entity);
+
+    public Task<T> Update(T entity);
+}
