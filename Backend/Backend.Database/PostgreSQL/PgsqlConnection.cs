ca1aa6584efdd19443ad243238803c6943a9708c
InVanSav <savickijivan11@gmail.com>
2023-07-21 02:29:22 +0300

    Connection string init

---
+++ Backend/Backend.Database/PostgreSQL/PgsqlConnection.cs
@@ -0,0 +1,41 @@
+namespace Backend.Database.PostgreSQL;
+
+/// <summary>
+///     Класс строки подключения базы данных PostgreSQL
+/// </summary>
+public class PgsqlConnection
+{
+    /// <summary>
+    ///     Название сервера
+    /// </summary>
+    public string? Server { get; set; }
+
+    /// <summary>
+    ///     Номер порта сервера
+    /// </summary>
+    public int Port { get; set; }
+
+    /// <summary>
+    ///     Название базы данных
+    /// </summary>
+    public string? Database { get; set; }
+
+    /// <summary>
+    ///     Имя владельца базой данных
+    /// </summary>
+    public string? User { get; set; }
+
+    /// <summary>
+    ///     Пароль базы данных
+    /// </summary>
+    public string? Password { get; set; }
+
+    /// <summary>
+    /// Получить строку подключения к базе данных
+    /// </summary>
+    /// <returns>Строку подключения к базе данных PostgreSQL</returns>
+    public string GetConnectionString()
+    {
+        return $"Server:{Server};Port:{Port};Database:{Database};User Id:{User};Password:{Password};";
+    }
+}
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,15 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+using Backend.Application.Repositories;
+using Backend.Database.Interfaces;
+using Backend.Database.PostgreSQL;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOptions<PgsqlConnection>()
+    .Bind(builder.Configuration.GetSection("Config:PgsqlConnection"));
+
+builder.Services.AddTransient<IAuctionRepository, IAuctionRepository>();
+builder.Services.AddTransient<IBetRepository, BetRepository>();
+builder.Services.AddTransient<ILotRepository, LotRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/error");
+
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
