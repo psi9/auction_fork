5c1520ec40d11e0527692c19417b996a9df39a78
Savickij Ivan <savickijivan11@gmail.com>
2023-07-27 08:31:57 +0300

    Use cases added to DI-Container

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,4 +1,9 @@
 using Backend.Application.Interfaces;
+using Backend.Application.UseCases.ChangeState;
+using Backend.Application.UseCases.CreateItems;
+using Backend.Application.UseCases.DeleteItems;
+using Backend.Application.UseCases.GetItems;
+using Backend.Application.UseCases.UpdateItems;
 using Backend.Database.PostgreSQL;
 using Backend.Database.Repositories;
 
@@ -12,12 +17,34 @@ builder.Services.AddOptions<PgsqlConnection>()
     .Bind(builder.Configuration.GetSection("Config:PgsqlConnection"));
 
 builder.Services.AddSingleton<IAuctionRepository, AuctionRepository>();
-builder.Services.AddSingleton<IBetRepository, BetRepository>();
 builder.Services.AddSingleton<ILotRepository, LotRepository>();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
-builder.Services.AddSingleton<IImageRepository, ImageRepository>();
 
 builder.Services.AddSingleton<PgsqlHandler>();
+
+builder.Services.AddSingleton<BuyoutLotHandler>();
+builder.Services.AddSingleton<ChangeAuctionStatusHandler>();
+builder.Services.AddSingleton<ChangeLotStatusHandler>();
+builder.Services.AddSingleton<DoBetHandler>();
+builder.Services.AddSingleton<SetDateEndAuctionHandler>();
+builder.Services.AddSingleton<SetDateStartAuctionHandler>();
+
+builder.Services.AddSingleton<CreateAuctionHandler>();
+builder.Services.AddSingleton<CreateLotHandler>();
+builder.Services.AddSingleton<CreateUserHandler>();
+
+builder.Services.AddSingleton<DeleteAuctionHandler>();
+builder.Services.AddSingleton<DeleteLotHandler>();
+builder.Services.AddSingleton<DeleteUserHandler>();
+
+builder.Services.AddSingleton<GetAuctionsHandler>();
+builder.Services.AddSingleton<GetAuctionByIdHandler>();
+builder.Services.AddSingleton<GetUsersHandler>();
+builder.Services.AddSingleton<GetUserByIdHandler>();
+
+builder.Services.AddSingleton<UpdateAuctionHandler>();
+builder.Services.AddSingleton<UpdateLotHandler>();
+builder.Services.AddSingleton<UpdateUserHandler>();
 
 var app = builder.Build();
 
