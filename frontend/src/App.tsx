e96fcdb815a7b3be48bb20fcf0d4494e3c2da0fd
Savickij Ivan <savickijivan11@gmail.com>
2023-08-02 12:43:39 +0300

    Some changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,49 @@
-import './App.css';
-import Header from "./components/header/Header"; 
-import Footer from "./components/footer/Footer"; 
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Arrow from "./components/arrow/Arrow";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
+      <Header />
+      <div className="main">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Arrow />
+      <Footer />
     </div>
   );
 }
