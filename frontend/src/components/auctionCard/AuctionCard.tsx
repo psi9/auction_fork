2427025036a62ea9aec4de1eed84436818a72f50
InVanSav <savickijivan11@gmail.com>
2023-08-03 10:46:47 +1000

    Code refactor

--- frontend/src/components/auctionCard/AuctionCard.tsx
+++ frontend/src/components/auctionCard/AuctionCard.tsx
@@ -9,11 +9,17 @@ export default function Card() {
       <div className="state">Статус: Ожидание</div>
       <div className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</div>
       <div className="container_info">
-        <div className="dateStart">Дата начала: 2021-12-21, 20:43</div>
-        <div className="dateEnd">Дата окончания: 2021-12-28, 10:20</div>
-        <div className="author">Автор: Держапольский Константин</div>
+        <div>
+          <div className="info_date">Дата начала:</div>  
+          <div className="date_text">2021-12-21, 20:43</div>
+        </div>
+        <div>
+          <div className="info_date">Дата конца:</div>  
+          <div className="date_text">2021-12-28, 10:20</div>
+        </div>
+        <div className="author">Держапольский Константин</div>
       </div>
-      <Button width="100%" text="Go to bidding" />
+      <Button width="100%" text="Принять участие" />
     </div>
   );
 }
