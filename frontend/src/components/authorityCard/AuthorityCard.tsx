cc093f5ac7e6a126d7bc04c21d543a4d2ac3f31e
Savickij Ivan <savickijivan11@gmail.com>
2023-08-04 10:58:32 +0300

    Created new component and its page

---
+++ frontend/src/components/authorityCard/AuthorityCard.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+import Button from "../button/Button";
+
+import "./AuthorityCard.css";
+
+export default function AuthorityCard() {
+  const [isSigned, setIsSigned] = useState<boolean>(true);
+
+  return (
+    <div className="authority_container">
+      <div className="container_buttons">
+        <button
+          className={`type ${isSigned ? "active" : ""}`}
+          onClick={() => setIsSigned(true)}
+        >
+          Авторизация
+        </button>
+        <button
+          className={`type ${!isSigned ? "active" : ""}`}
+          onClick={() => setIsSigned(false)}
+        >
+          Регистрация
+        </button>
+      </div>
+      {isSigned ? (
+        <div className="container_inputs">
+          <input
+            className="input_item"
+            type="text"
+            name="login"
+            placeholder="Введите логин"
+          ></input>
+          <input
+            className="input_item"
+            type="email"
+            name="email"
+            placeholder="example@mail.ru"
+          ></input>
+        </div>
+      ) : (
+        <div className="container_inputs">
+          <input
+            className="input_item"
+            type="text"
+            name="login"
+            placeholder="Введите логин"
+          ></input>
+          <input
+            className="input_item"
+            type="email"
+            name="email"
+            placeholder="example@mail.ru"
+          ></input>
+          <input
+            className="input_item"
+            type="password"
+            name="password"
+            placeholder="Введите пароль"
+          ></input>
+          <input
+            className="input_item"
+            type="password"
+            name="repeat_password"
+            placeholder="Повторите пароль"
+          ></input>
+        </div>
+      )}
+
+      <Button width="100%" text="Подтвердить" />
+    </div>
+  );
+}
---
+++ frontend/src/pages/AuthorityPage/AuthorityPage.tsx
@@ -0,0 +1,11 @@
+import AuthorityCard from "../../components/authorityCard/AuthorityCard";
+
+import "./AuthorityPage.css";
+
+export default function AuthorityPage() {
+  return (
+    <div className="main">
+      <AuthorityCard />
+    </div>
+  );
+}
